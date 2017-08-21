ae01125d451c634b376a654e109ad8f4e00bc7e9
Sanjay Agnani <sanjay.agnani@Aardhys-MacBook-Pro-2.local>
2017-08-21 15:46:43 +0530

    Desktop_

--- A.java
+++ A.java
@@ -10,7 +10,7 @@ public class A
 }
 
 
-	
+	/Users/sanjay.agnani/Downloads/users.sql
 Rahil Parikh	8:21 AM 19 August
 https://www.tutorialspoint.com/testng/index.htm
 https://www.tutorialspoint.com/json/index.htm
@@ -21,3 +21,16 @@ https://www.tutorialspoint.com/servlets/index.htm
 https://www.tutorialspoint.com/jsp/index.htm
 
 http://www.studytonight.com/jsp/jsp-declaration-tag.php
+
+
+21 Aug:
+https://stackoverflow.com/questions/11707879/difference-between-scaling-horizontally-and-vertically-for-databases
+
+http://blog.rdx.com/cassandra-and-relational-database-schema-comparison-query-vs-relationship-modeling/
+
+Properties: /usr/local/etc/cassandra
+Logs: /usr/local/var/log/cassandra
+Data: /usr/local/var/lib/cassandra/data
+
+CQLSH COMMANDS:
+https://vexxhost.com/resources/tutorials/how-to-setup-cassandra-and-run-a-single-node-cluster-on-ubuntu-16-04/
