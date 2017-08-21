49415d22070835a5ba7be907d7d8ff95cc573860
Sanjay Agnani <sanjayagnani92@gmail.com>
2017-08-21 08:25:11 +0530

    Initial Commit

---
+++ A.java
@@ -0,0 +1,23 @@
+public class A
+{
+    
+    public static void main(String args[])
+    {
+        System.out.println("A");
+        
+               
+    }
+}
+
+
+	
+Rahil Parikh	8:21 AM 19 August
+https://www.tutorialspoint.com/testng/index.htm
+https://www.tutorialspoint.com/json/index.htm
+https://www.tutorialspoint.com/junit/index.htm
+https://www.tutorialspoint.com/log4j/index.htm
+https://www.tutorialspoint.com/maven/index.htm
+https://www.tutorialspoint.com/servlets/index.htm
+https://www.tutorialspoint.com/jsp/index.htm
+
+http://www.studytonight.com/jsp/jsp-declaration-tag.php
